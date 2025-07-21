a01c94c5d94b5b5c75056b1f958e34290df6c49c
25063199 <25063199@imail.sunway.edu.my>
2025-07-21 06:00:55 +0000

    done

---
+++ Part1/ex03.py
@@ -0,0 +1,4 @@
+string1 = "MaRvIn THE bOt"
+string_lower = print(string1.lower())
+string_upper = print(string1.upper())
+string_nospace = print(string1.replace(" ",""))
---
+++ Part1/ex07.py
@@ -0,0 +1,7 @@
+age = {"Hanz": 24, "Prag": 23, "Bunyod": 18}
+print(age)
+print(age["Hanz"])
+age["Prag"] = 30
+print(age)
+del age["Bunyod"]
+print(age)
